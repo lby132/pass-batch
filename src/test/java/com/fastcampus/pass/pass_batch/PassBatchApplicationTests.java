2b03bb196bb387ba847879a9c9ba03b9290d0de7
lby132 <quddbscjswow@naver.com>
2024-09-11 12:58:43 +0900

    first commit

---
+++ src/test/java/com/fastcampus/pass/pass_batch/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.pass.pass_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
