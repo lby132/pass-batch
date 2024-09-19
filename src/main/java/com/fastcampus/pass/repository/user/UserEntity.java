074878b44d93e83771e0de631a9ac40528afed96
lby132 <quddbscjswow@naver.com>
2024-09-19 19:47:09 +0900

    booking 엔티티 연관관계 수정, User엔티티 meta 를 map으로 받을수 있게 수정

--- src/main/java/com/fastcampus/pass/repository/user/UserEntity.java
+++ src/main/java/com/fastcampus/pass/repository/user/UserEntity.java
@@ -5,6 +5,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Map;
 
 
 @Getter
@@ -21,8 +25,21 @@ public class UserEntity extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private UserStatus status;
-
     private String phone;
-    private String meta;
+
+    // json 형태로 저장되어 있는 문자열 데이터를 Map으로 매핑
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> meta;
+
+    public String getUuid() {
+        String uuid = null;
+        if (meta.containsKey("uuid")) {
+            uuid = String.valueOf(meta.get("uuid"));
+        }
+        return uuid;
+    }
+
+
+
 
 }
