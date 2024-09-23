9b61b8b96871f5de8b1ee473f6d19d9d627400a3
lby132 <quddbscjswow@naver.com>
2024-09-23 21:26:32 +0900

    수업 종료후 이용권 차감 AsyncItemProcessor, AsyncItemWriter 사용

---
+++ src/main/java/com/fastcampus/pass/job/pass/UsePassesJobConfig.java
@@ -0,0 +1,115 @@
+package com.fastcampus.pass.job.pass;
+
+import com.fastcampus.pass.repository.booking.BookingEntity;
+import com.fastcampus.pass.repository.booking.BookingRepository;
+import com.fastcampus.pass.repository.booking.BookingStatus;
+import com.fastcampus.pass.repository.pass.PassEntity;
+import com.fastcampus.pass.repository.pass.PassRepository;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.integration.async.AsyncItemProcessor;
+import org.springframework.batch.integration.async.AsyncItemWriter;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+@Configuration
+@RequiredArgsConstructor
+public class UsePassesJobConfig {
+    private final int CHUNK_SIZE = 10;
+
+    // @EnableBatchProcessing로 인해 Bean으로 제공된 JobBuilderFactory, StepBuilderFactory
+    private final EntityManagerFactory entityManagerFactory;
+    private final PassRepository passRepository;
+    private final BookingRepository bookingRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+
+    @Bean
+    public Job usePassesJob(JobRepository jobRepository) {
+        return new JobBuilder("usePassesJob", jobRepository)
+                .start(usePassesStep(jobRepository))
+                .build();
+    }
+
+    @Bean
+    public Step usePassesStep(JobRepository jobRepository) {
+        return new StepBuilder("usePassesStep", jobRepository)
+                .<BookingEntity, Future<BookingEntity>>chunk(CHUNK_SIZE, platformTransactionManager)
+                .reader(usePassesItemReader())
+                .processor(usePassesAsyncItemProcessor())
+                .writer(usePassesAsyncItemWriter())
+                .build();
+
+    }
+
+    @Bean
+    public JpaCursorItemReader<BookingEntity> usePassesItemReader() {
+        return new JpaCursorItemReaderBuilder<BookingEntity>()
+                .name("usePassesItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                // 상태(status)가 완료이며, 종료 일시(endedAt)이 과거인 예약이 이용권 차감 대상이 됨
+                .queryString("select b from BookingEntity b join fetch b.passEntity where b.status = :status and b.usedPass = false and b.endedAt < :endedAt")
+                .parameterValues(Map.of("status", BookingStatus.COMPLETED, "endedAt", LocalDateTime.now()))
+                .build();
+    }
+
+    // 이 프로젝트에서는 적합하지 않지만, ItemProcessor의 수행이 오래걸려 병목이 생기는 경우에 AsyncItemProcessor, AsyncItemWriter를 사용하면 성능을 향상시킴
+    @Bean
+    public AsyncItemProcessor<BookingEntity, BookingEntity> usePassesAsyncItemProcessor() {
+        AsyncItemProcessor<BookingEntity, BookingEntity> asyncItemProcessor = new AsyncItemProcessor<>();
+        asyncItemProcessor.setDelegate(usePassesItemProcessor()); // usePassesItemProcessor로 위임하고 결과를 Future에 저장합니다.
+        asyncItemProcessor.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return asyncItemProcessor;
+
+    }
+
+    @Bean
+    public ItemProcessor<BookingEntity, BookingEntity> usePassesItemProcessor() {
+        return bookingEntity -> {
+            // 이용권 잔여 횟수는 차감
+            PassEntity passEntity = bookingEntity.getPassEntity();
+            passEntity.setRemainingCount(passEntity.getRemainingCount() - 1);
+            bookingEntity.setPassEntity(passEntity);
+
+            // 이용권 사용 여부를 업데이트
+            bookingEntity.setUsedPass(true);
+            return bookingEntity;
+
+        };
+    }
+
+    @Bean
+    public AsyncItemWriter<BookingEntity> usePassesAsyncItemWriter() {
+        AsyncItemWriter<BookingEntity> asyncItemWriter = new AsyncItemWriter<>();
+        asyncItemWriter.setDelegate(usePassesItemWriter()); // usePassesItemWriter 최종 결과값을 넘겨주고 작업을 위임합니다.
+        return asyncItemWriter;
+    }
+
+    @Bean
+    public ItemWriter<BookingEntity> usePassesItemWriter() {
+        return bookingEntities -> {
+            for (BookingEntity bookingEntity : bookingEntities) {
+                // 잔여 횟수를 업데이트
+                int updatedCount = passRepository.updateRemainingCount(bookingEntity.getPassSeq(), bookingEntity.getPassEntity().getRemainingCount());
+                // 잔여 횟수가 업데이트 완료되면, 이용권 사용 여부를 업데이트
+                if (updatedCount > 0) {
+                    bookingRepository.updateUsedPass(bookingEntity.getPassSeq(), bookingEntity.isUsedPass());
+                }
+            }
+        };
+    }
+}
